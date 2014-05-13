4b8bd751de2004f15fa52779a90a487e03e82cc1
Miguel Lopes <miguel@miguellopes.net>
2014-05-13 18:50:20 +0100

    debian install

--- setup.py
+++ setup.py
@@ -1,30 +1,24 @@
 import sys, os, shutil
-from distutils.core import setup, Extension
+from distutils.core import setup
 
 if sys.argv[1] != "sdist":
     shutil.copyfile("knockknock-daemon.py", "knockknock/knockknock-daemon")
     shutil.copyfile("knockknock-genprofile.py", "knockknock/knockknock-genprofile")
-    shutil.copyfile("knockknock-proxy.py", "knockknock/knockknock-proxy")
-    shutil.copyfile("knockknock.py", "knockknock/knockknock")
 
-setup  (name         = 'knockknock',
+
+setup (name         = 'knockknock',
         version      = '0.8',
         description  = 'A cryptographic single-packet port-knocker.',
         author       = 'Moxie Marlinspike',
         author_email = 'moxie@thoughtcrime.org',
         url          = 'http://www.thoughtcrime.org/software/knockknock/',
         license      = 'GPL',
-        packages     = ["knockknock", "knockknock.proxy"],
+        install_requires=['crypto'],
+        packages     = ['knockknock'],
         scripts      = ['knockknock/knockknock-daemon',
-                        'knockknock/knockknock-genprofile',
-                        'knockknock/knockknock-proxy',
-                        'knockknock/knockknock'],
-        data_files   = [("", ["minimal-firewall.sh", "knockknock-daemon.py", 
-                              "knockknock-genprofile.py", "knockknock-proxy.py", 
-                              "knockknock.py"]),
-                        ('share/knockknock', ['README', 'INSTALL', 'COPYING']),
-                        ('/etc/knockknock.d/', ['config'])]
-       )
+            'knockknock/knockknock-genprofile'],
+        data_files   = [('/usr/share/knockknock', ['README', 'INSTALL', 'COPYING']), ('/etc/knockknock.d', ['config']), ('/etc/init.d', ['service/knockknock'])]
+        )
 
 print "Cleaning up..."
 
@@ -32,10 +26,8 @@ if os.path.exists("build/"):
     shutil.rmtree("build/")
 
 try:
-    os.remove("knockknock/knockknock-proxy")
     os.remove("knockknock/knockknock-daemon")
     os.remove("knockknock/knockknock-genprofile")
-    os.remove("knockknock/knockknock")
 
 except:
     pass
